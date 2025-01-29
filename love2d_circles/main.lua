5b110e8b06208cd171e69f4de3d66816e22b0c27
Alan Silva <alanfortlink@gmail.com>
2025-01-29 19:04:42 +0000

    update

--- love2d_circles/main.lua
+++ love2d_circles/main.lua
@@ -1,60 +1,63 @@
 local world
 local objects
 
-local num_circles = 1500
-local radius = 4
-local fac = 8
-local side = 800
+local num_circles = 1000
+local radius = 2
 
-local restitution = 1.0
-local boundary_restitution = 1.0
+local side = 800
+local min_force = 10
+local max_force = 20
+
+local random_sign = function()
+  if math.random() > 0.5 then
+     return 1
+  end
+  return -1
+end
 
 function love.load()
   love.physics.setMeter(64)
   love.window.setMode(side, side, { fullscreen = false })
   world = love.physics.newWorld(0, 0, true)
-  objects = {}
-
-  objects.balls = {}
+  objects = { balls = {} }
 
   for _ = 1, num_circles do
     local ball = {}
     ball.body = love.physics.newBody(world, math.random(0, side), math.random(0, side), "dynamic")
     ball.shape = love.physics.newCircleShape(radius)
     ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
-    ball.fixture:setRestitution(restitution)
-    ball.body:applyForce(math.random(-radius * fac, radius * fac), math.random(-radius * fac, radius * fac))
+    ball.fixture:setRestitution(1.0)
+    ball.body:applyForce(
+      random_sign() * math.random(min_force, max_force),
+      random_sign() * math.random(min_force, max_force)
+    )
     ball.color = { r = math.random(), g = math.random(), b = math.random() }
     table.insert(objects.balls, ball)
   end
 
-  objects.ground = {}
-  objects.ground.body = love.physics.newBody(world, side / 2, side + 50 / 2)
-  objects.ground.shape = love.physics.newRectangleShape(side, 50)
-  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
-  objects.ground.fixture:setRestitution(boundary_restitution)
-
-  objects.ceiling = {}
-  objects.ceiling.body = love.physics.newBody(world, side / 2, -50 / 2)
-  objects.ceiling.shape = love.physics.newRectangleShape(side, 50)
-  objects.ceiling.fixture = love.physics.newFixture(objects.ceiling.body, objects.ceiling.shape)
-  objects.ceiling.fixture:setRestitution(boundary_restitution)
-
-  objects.left = {}
-  objects.left.body = love.physics.newBody(world, -50 / 2, side / 2)
-  objects.left.shape = love.physics.newRectangleShape(50, side)
-  objects.left.fixture = love.physics.newFixture(objects.left.body, objects.left.shape)
-  objects.left.fixture:setRestitution(boundary_restitution)
-
-  objects.right = {}
-  objects.right.body = love.physics.newBody(world, side + 50 / 2, side / 2)
-  objects.right.shape = love.physics.newRectangleShape(50, side)
-  objects.right.fixture = love.physics.newFixture(objects.right.body, objects.right.shape)
-  objects.right.fixture:setRestitution(boundary_restitution)
+  do
+    objects.border = love.physics.newBody(world, 0, 0, "static")
+    local shape = love.physics.newChainShape(true, {
+      0, 0,       --
+      side, 0,    --
+      side, side, --
+      0, side     --
+    })
+    local fixture = love.physics.newFixture(objects.border, shape)
+    fixture:setRestitution(1.0)
+  end
 end
 
 function love.update(dt)
   world:update(dt)
+  local random_factor = 0.1
+  for _, ball in ipairs(objects.balls) do
+    -- add randomness
+    ball.body:applyForce(
+      random_factor * random_sign() * math.random(min_force, max_force),
+      random_factor * random_sign() * math.random(min_force, max_force)
+    )
+  end
 end
 
 function love.draw()
